87451736480943a8f4119c94d22254e9b4b70d24
kaktos <sticknumber123@gmail.com>
2024-07-10 04:15:37 +0330

    Remove unused classes

--- src/SqlParser/Old/Token3.cs
+++
@@ -1,9 +0,0 @@
-namespace SqlParser;
-
-public class Token3
-{
-    public required TokenType3 TokenType3 { get; init; }
-    public char? ValueChar { get; init; }
-    public int? ValueInt { get; init; }
-    public decimal? ValueDouble { get; init; }
-}
