dca6844f59e1fbf14fb71009eb342fc1d08934ff
kaktos <sticknumber123@gmail.com>
2024-07-16 19:21:17 +0330

    Add comments

--- src/SqlParser/Program.cs
+++ src/SqlParser/Program.cs
@@ -1,3 +1,7 @@
+// See https://www.youtube.com/watch?v=5Pc18ge9ohI&ab_channel=TonySaro
+// https://ruslanspivak.com/lsbasi-part1/
+// https://forcedotcom.github.io/phoenix/index.html#select_expression
+
 using SqlParser;
 
 var input = """
